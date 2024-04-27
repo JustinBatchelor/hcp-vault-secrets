e8dced6e93d937ff37b4a310c41975d8ad1468fe
Justin Batchelor <jbatchel@jb-pc.lan>
2024-04-27 00:30:48 -0400

    updating docs

--- tests/getSecrets.py
+++ tests/getSecrets.py
@@ -1,13 +1,13 @@
 import sys, os
 ## Code to disable creating pycache dir after running
 sys.dont_write_bytecode = True
+
 sys.path.append(os.path.abspath(f"{os.getcwd()}/src/"))
 
-from hcp_vault_secrets.vaultsecrets import vaultsecrets
+import hcp_vault_secrets.vaultsecrets as vaultsecrets
 
-hcp = vaultsecrets()
+hcp = vaultsecrets.vaultsecrets()
 
 prox = hcp.getAppSecret("proxmox", "password")
-
 
 print(prox)
